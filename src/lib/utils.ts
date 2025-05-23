600f763720f503e1d559e5907cdf46b7a758a628
Chandan Kumar <chandankrr.91@gmail.com>
2025-05-23 12:25:02 +0530

    fix: deployment issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,8 +6,12 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantURL(tenantSlug: string) {
+	const isDevelopment = process.env.NODE_ENV === "development";
+	const isSubdomainRoutingEnabled =
+		process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING! === "true";
+
 	// In development mode, use normal routing
-	if (process.env.NODE_ENV === "development") {
+	if (isDevelopment || !isSubdomainRoutingEnabled) {
 		return `${process.env.NEXT_PUBLIC_APP_URL!}/tenants/${tenantSlug}`;
 	}
 
